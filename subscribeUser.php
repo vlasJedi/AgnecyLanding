7093bc2db164d7412484a3c747cc6dd95e88ddc3
vlasJedi <vlas.d78@gmail.com>
2018-10-23 15:39:14 +0300

    added server conf

---
+++ subscribeUser.php
@@ -0,0 +1,17 @@
+<?php
+echo<<<HTML
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+		<title>Agnecy</title>
+		<link rel="stylesheet" href="css/bootstrap-grid.min.css" >
+		<link rel="stylesheet" href="css/main.css" type="text/css" >
+	</head>
+	<body>
+	<h1> Sayonara !</h1>
+	</body>
+	</html>
+HTML;
+	?>
