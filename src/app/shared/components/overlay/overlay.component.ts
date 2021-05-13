9b7c063940d296139ce0d130afdf43b9c21073c2
Vlas.Dielov <503118341@ge.com>
2021-05-13 20:43:20 +0300

    overlay-added

---
+++ src/app/shared/components/overlay/overlay.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'overlay',
+  templateUrl: './overlay.component.html',
+  styleUrls: ['./overlay.component.scss']
+})
+export class OverlayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getVisible() {
+      return false;
+  }
+
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { RouterModule } from '@angular/router';
 import { OverflowedDirective } from './directives/overflowed.directive';
+import { OverlayComponent } from './components/overlay/overlay.component';
 
 
 
 @NgModule({
-  declarations: [NavigationComponent, OverflowedDirective],
+  declarations: [NavigationComponent, OverflowedDirective, OverlayComponent],
   imports: [
     // this router need to be imported as it contains definition for routerLink directive
     CommonModule, RouterModule
   ],
-  exports: [NavigationComponent]
+  exports: [NavigationComponent, OverlayComponent]
 })
 export class SharedModule { }
